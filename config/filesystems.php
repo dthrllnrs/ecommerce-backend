bb7771f5bb8bdb52dbd5f817c75f77413de5fcc1
Diether <llenaresdiether@gmail.com>
2023-12-26 13:07:51 +0800

    add public filesystem disk

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,8 +38,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => storage_path('app/public/storage/images'),
+            'url' => env('APP_URL').'/storageimages',
             'visibility' => 'public',
             'throw' => false,
         ],
